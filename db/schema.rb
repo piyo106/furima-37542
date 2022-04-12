de2a2f6c64fb2c1b66a481b34be556107ef06de1
piyo106 <wolkige.mond@gmail.com>
2022-04-12 23:49:32 +0900

    usersテーブルのカラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema.define(version: 2022_04_12_065807) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "family_name_kanji", null: false
-    t.string "first_name_kanji", null: false
-    t.string "family_name_kana", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
     t.date "birthday", null: false
     t.string "reset_password_token"
