f217eda7ae4da2c60b5538ac449e81877053f390
piyo106 <wolkige.mond@gmail.com>
2022-04-13 11:36:38 +0900

    重複したエラーメッセージの表示を回避

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true } do
     validates :first_name
     validates :last_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true } do
     validates :last_name_kana
     validates :first_name_kana
   end
