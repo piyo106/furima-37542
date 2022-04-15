4bea10bc1b526725b508174b9d567ca65c96e460
piyo106 <wolkige.mond@gmail.com>
2022-04-15 15:37:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  validates :name, :description, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }
+
   belongs_to :user
   has_one_attached :image
 
