82252ee323d49535cb916eda41601daf1ec3d1b2
piyo106 <wolkige.mond@gmail.com>
2022-04-15 15:28:40 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
 end
