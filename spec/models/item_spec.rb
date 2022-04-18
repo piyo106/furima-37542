05c53f49c780a4a08cf725d4acf110bc96c6372b
piyo106 <wolkige.mond@gmail.com>
2022-04-18 15:39:44 +0900

    Rubocop指摘箇所修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -69,7 +69,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Price must be greater than or equal to 300"
       end
       it '商品価格が9,999,999円以上では保存できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include "Price must be less than or equal to 9999999"
       end
