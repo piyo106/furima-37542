34d2f41783a0dd7ec9e9bd5a9972212ff1b53693
piyo106 <wolkige.mond@gmail.com>
2022-04-21 14:27:45 +0900

    購入フォームの情報を保存可能に実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,23 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
+  end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -1,19 +1,19 @@
 class OrderShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture, :municipality, :address, :building, :tel, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building, :tel, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code, format: { {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}, allow_blank: true }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)", allow_blank: true }
     validates :municipality
     validates :address
-    validates :tel, format: { /\A\d{10,11}\z/, message: "is invalid. Input half-width-digit", allow_blank: true }
+    validates :tel, format: { with: /\A\d{10,11}\z/, message: "is invalid. Input half-width-digit", allow_blank: true }
     validates :user_id
     validates :item_id
   end
-  validates :prefecture, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    ShippingAddress.create(postal_code: postal_code, prefecture: prefecture, municipality: municipality, address: address, building: building, tel: tel, order_id: order.id)
+    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, tel: tel, order_id: order.id)
   end
 end
