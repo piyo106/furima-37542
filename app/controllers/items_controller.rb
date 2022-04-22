19198ec641517c9908e4315e23f6cfa5c5be4742
piyo106 <wolkige.mond@gmail.com>
2022-04-22 22:44:09 +0900

    購入歴に応じてページ遷移や表示を変更する処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id != @item.user_id
+    if current_user.id != @item.user_id || @item.order.present?
       redirect_to action: :index
     end
   end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,7 +27,7 @@ class OrdersController < ApplicationController
 
   def move_to_index
     item = Item.find(params[:item_id])
-    if current_user.id == item.user_id
+    if current_user.id == item.user_id || item.order.present?
       redirect_to root_path
     end
   end
