95ca93add852933fe1f5d9fb9cec240b183508d8
piyo106 <wolkige.mond@gmail.com>
2022-04-23 22:04:20 +0900

    重複したコードをまとめる処理

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,9 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
   before_action :move_to_index
 
   def index
-    @item = Item.find(params[:item_id])
     @order_shipping_address = OrderShippingAddress.new
   end
 
@@ -14,7 +14,6 @@ class OrdersController < ApplicationController
       @order_shipping_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -25,15 +24,17 @@ class OrdersController < ApplicationController
     params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :municipality, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def move_to_index
-    item = Item.find(params[:item_id])
-    if current_user.id == item.user_id || item.order.present?
+    if current_user.id == @item.user_id || @item.order.present?
       redirect_to root_path
     end
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
